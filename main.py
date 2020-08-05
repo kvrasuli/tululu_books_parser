c62a2ae766e30f32e9f2f2951b75334055536993
Kirill Rasuli <rasulikv@gmail.com>
2020-08-05 19:22:20 +0300

    Download book with titles

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 import requests
-from itertools import count
 from pathlib import Path
 from bs4 import BeautifulSoup
 import lxml
-
+from pathvalidate import sanitize_filename
 
 def main():
-    Path('books').mkdir(parents=True, exist_ok=True)
-    for id in count(1):
-        url = f'http://tululu.org/txt.php?id={id}'
-        response = requests.get(url)
-        response.raise_for_status()
-        if response.url != 'http://tululu.org/':
-            author, title = get_author_and_title(id)
-            print(author, title)
-            with open(f'books/{title}_{author}.txt', 'wb') as book:
-                book.write(response.content)
+    for book_id in range(1, 11):
+        url = f'http://tululu.org/txt.php?id={book_id}'
+        title = get_title(book_id)
+        if title is not None:
+            download_txt(url, f'{book_id}. {title}')
 
-        if id == 10: 
-            break
-
-
-def get_author_and_title(id):
-    response = requests.get(f'http://tululu.org/b{id}/')
+def get_title(book_id):
+    title = None
+    response = requests.get(f'http://tululu.org/b{book_id}/')
     response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    author = soup.find('h1').text.split('::')[0].strip()
-    title = soup.find('h1').text.split('::')[1].strip()
-    return author, title
+    if response.url != 'http://tululu.org/':
+        soup = BeautifulSoup(response.text, 'lxml')
+        title = soup.find('h1').text.split('::')[0].strip()
+    return title
+
+def download_txt(url, filename, folder='books/'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    if response.url != 'http://tululu.org/':
+        sanitized_filename = sanitize_filename(filename)
+        path_to_save = Path(folder).joinpath(f'{sanitized_filename}.txt')
+        with open(path_to_save, 'wb') as book:
+            book.write(response.content)
+        return path_to_save
+
+
+
+
 
 if __name__ == '__main__':
     main()
